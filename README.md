ac0fdfa598000b917fc4ee53de214137208ff68b
Angelo Poerio <angelo.poerio@gmail.com>
2024-02-18 21:29:47 +0100

    better comments in code & README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # mlflow-for-mlops
+This repo show how to use MLFlow to track end to end a machine learning project
+
+# Howto
+
--- experiment.py
+++ experiment.py
@@ -15,7 +15,7 @@ def experiment_exists_or_create(name):
     experiment = client.get_experiment_by_name(name)
     if experiment:
         return experiment
-    return client.create_experiment(name=name, tags={})
+    return client.create_experiment(name=name, tags={})  # set tags here as you prefer
 
 
 if __name__ == "__main__":
@@ -26,19 +26,20 @@ if __name__ == "__main__":
     iris = datasets.load_iris()
     X = iris.data
     Y = iris.target
+    test_size = 0.2
 
     X_train, X_val, y_train, y_val = train_test_split(
-        X, Y, test_size=0.2, random_state=42
+        X, Y, test_size=test_size, random_state=42
     )
 
-    C = 0.1
+    C = 0.1  # See: https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html#sklearn-linear-model-logisticregression
     logreg = LogisticRegression(C=C)
     logreg.fit(X, Y)
     y_pred = logreg.predict(X_val)
     accuracy = accuracy_score(y_val, y_pred)
 
     with mlflow.start_run(run_name=RUN_NAME) as run:
-        mlflow.log_params({"C": C})
+        mlflow.log_params({"C": C, "test_size": test_size})
         mlflow.log_metrics({"accuracy": accuracy})
         mlflow.sklearn.log_model(
             sk_model=logreg, input_example=X_val, artifact_path=ARTIFACT_PATH
